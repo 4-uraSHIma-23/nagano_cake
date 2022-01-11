73746c06d62950083eecee680b469588366f9b60
4-uraSHIma-23 <nicorulove423@gmail.com>
2022-01-11 16:56:01 +0000

    admin_genre_index Finish

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -1,7 +1,22 @@
 class Admin::GenresController < ApplicationController
   def index
+    @list = List.new
+    @lists = List.all
+  end
+
+  def create
+    list = List.new(list_params)
+    list.save
+    redirect_to '/admin/genres'
   end
 
   def edit
   end
+
+  private
+
+  def list_params
+    params.require(:genre).permit(:name)
+  end
+
 end
